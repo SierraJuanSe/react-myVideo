714fe09c80bbb7fa1bec01e98c5e608c250ae6fc
Sebastian Sierra <juansebastiansierrac@gmail.com>
2021-04-11 14:19:14 -0500

    Hooks and CustomHooks for fake APO

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -5,35 +5,35 @@ import Categories from '@components/Categories.jsx';
 import Carousel from '@components/Carousel.jsx';
 import CarouselItem from '@components/CarouselItem.jsx';
 import Footer from '@components/Footer.jsx';
+import useInitialState from '../hooks/useInitialState';
 import '@styles/App.scss';
 
+const API = 'http://localhost:3000/initalState';
+
 const App = () => {
-  return (
+  const initialState = useInitialState(API);
+  return initialState.length === 0 ? <h1>Loading...</h1> : (
     <div className='App'>
       <Header />
       <Search />
-      <Categories title='Mi lista'>
+
+      {initialState.mylist.length > 0 && (
+        <Categories title='My List'>
+          <Carousel>
+            {initialState.mylist.map((video) => <CarouselItem key={video.id} {...video} />)}
+          </Carousel>
+        </Categories>
+      )}
+
+      <Categories title='Trends'>
         <Carousel>
-          <CarouselItem />
-          <CarouselItem />
-          <CarouselItem />
-          <CarouselItem />
+          {initialState.trends.map((video) => <CarouselItem key={video.id} {...video} />)}
         </Carousel>
       </Categories>
-      <Categories title='Tendencias'>
+
+      <Categories title='Originals'>
         <Carousel>
-          <CarouselItem />
-          <CarouselItem />
-          <CarouselItem />
-          <CarouselItem />
-        </Carousel>
-      </Categories>
-      <Categories title='Nuevo'>
-        <Carousel>
-          <CarouselItem />
-          <CarouselItem />
-          <CarouselItem />
-          <CarouselItem />
+          {initialState.originals.map((video) => <CarouselItem key={video.id} {...video} />)}
         </Carousel>
       </Categories>
       <Footer />
